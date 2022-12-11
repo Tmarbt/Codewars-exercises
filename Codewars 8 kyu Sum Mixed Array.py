aa015aa443589800107c54e38fa1323bce56dd59
Artem Stoliar <99268175+Tmarbt@users.noreply.github.com>
2022-12-11 20:21:04 +0100

    Create Codewars 8 kyu Sum Mixed Array.py

---
+++ Codewars 8 kyu Sum Mixed Array.py
@@ -0,0 +1,9 @@
+# 8 kyu Sum Mixed Array
+'''
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+'''
+def sum_mix(arr):
+    return sum([int(i) for i in arr])
