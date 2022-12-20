fa7d509d5b838725a46c24a340c36f1e88992280
Artem Stoliar <99268175+Tmarbt@users.noreply.github.com>
2022-12-20 21:31:25 +0100

    Create Codewars 8 kyu Remove First and Last Character.py

---
+++ Codewars 8 kyu Remove First and Last Character.py
@@ -0,0 +1,8 @@
+# 8 kyu Remove First and Last Character
+'''
+DESCRIPTION:
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+'''
+def remove_char(s):
+    result = s[1:-1]
+    return result
